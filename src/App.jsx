1a8a3bba6e4e457abef5fc8d7fa501d06bb91ffc
Espen Luna <espensluna@gmail.com>
2024-09-26 15:25:15 +0200

    Core & Extensions

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,92 @@
+import { useState } from "react"
 import "./App.css"
 
 function App() {
+  
+  let numberOptions = [1,2,3,4,5,6,7,8,9,0, '.', 'ANS']
+  let operatorOptions = ['+', '-', '*', '/']
+
+  const [values, setValues] = useState({
+    firstNumber: null,
+    operator: '+',
+    secondNumber: null,
+    result: 0,
+  })
+  
+  const [stored, setStored] = useState(null)
+
+  function clear(e){
+    setValues(prev => ({
+      ...prev,
+      [e]: null
+    }))
+  }
+
+  function calculate(){
+    let first = values.firstNumber ? values.firstNumber : 0
+    let second = values.secondNumber ? values.secondNumber : 0
+    let results = (eval(first + values.operator + second));
+    setValues(prev => ({
+      ...prev,
+      result: results
+    }))
+  }
+
+  function handleInput(event){
+    setValues(prev => ({
+      ...prev,
+      [event.target.name]: (event.target.name === 'operator' || prev[event.target.value] === '0' ) ? event.target.value : 
+      (event.target.value === 'ANS' ) ? stored :
+      (
+      (prev[event.target.name] === null || prev[event.target.name] === '0' ) ? event.target.value :
+      
+      `${prev[event.target.name] + event.target.value}`
+      )
+    }))
+  }
 
   return (
     <div className="calculator">
       <div className="panel">
-        <p>0</p>
+        <p>{values.firstNumber ? values.firstNumber : 0}</p>
         <div className="numbers">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-          <button>Clear</button>
+          {numberOptions.map((n) => (
+            <>
+              <button name="firstNumber" value={n} onClick={handleInput}>{n}</button>
+            </>
+          ))}
+          <button onClick={() => clear('firstNumber')}>Clear</button>
+
         </div>
       </div>
 
       <div className="panel">
-        <p>+</p>
+        <p>{values.operator}</p>
         <div className="numbers">
-          <button>+</button>
-          <button>-</button>
-          <button>*</button>
-          <button>÷</button>
+          {operatorOptions.map((o) => (
+            <>
+              <button name="operator" value={o} onClick={handleInput}>{o}</button>
+            </>
+          ))}
         </div>
       </div>
 
       <div className="panel">
-        <p>0</p>
+        <p>{values.secondNumber ? values.secondNumber : 0}</p>
         <div className="numbers">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-          <button>Clear</button>
+          {numberOptions.map((n) => (
+            <>
+            <button name="secondNumber" value={n} onClick={handleInput}>{n}</button>
+            </>
+          ))}
+          <button onClick={() => clear('secondNumber')}>Clear</button>
         </div>
       </div>
       <div className="panel answer">
-        <p>0</p>
+        <p>{values.result}</p>
         <div>
-          <button>=</button>
+          <button onClick={() => calculate()}>=</button>
+          <button onClick={() => setStored(values.result)}>STORE</button>
         </div>
       </div>
     </div>
